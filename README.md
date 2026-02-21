9e9c0dd5bcb64c026d11fab4f0bf6581cef9276e
jake-young-dev <17075259+jake-young-dev@users.noreply.github.com>
2026-02-21 01:49:04 -0500

    rewrites
    - adding more methods for more control over the bot
    - login and listening are decoupled
    - moved to a structure to allow for more events in the future

--- README.md
+++ README.md
@@ -1,9 +1,63 @@
 # tsgo
 
-tsgo is a TeamSpeak 3 bot framework that utilizes the Server Query protocol to support text commands, tsgo is in development and may
-encounter breaking changes until v1 release
+tsgo is a bot framework for TeamSpeak 3 servers utilizing the Server Query protocol. Enabling easy creation of text-based bots that support
+custom commands. As of right now, this framework is very basic and intended for basic bots, it is in development and may encounter breaking
+changes until a v1 release.
+
+## Documentation
+### Config
+A basic configuration struct is provided for bots
+```go
+type Config struct {
+	//the server address and port of the TeamSpeak server
+	Address string
+	Port    string
+	//TeamSpeak credentials to authenticate the bot in the server
+	Username string
+	Password string
+	//the server identifier to be used in the 'use' command, this tells the
+	//bot which server to listen to. TeamSpeak supports a port option as the identifier but it is not supported yet, this must be the Server ID
+	Server int
+}
+```
+
+### Handlers
+Currently the only handler supported by tsgo is the message handler, a message is defined as:
+```go
+// Message is the parsed message from a notifytextmessage event, this
+// struct is passed to the message handler functions
+type Message struct {
+	Msg         string
+	InvokerID   int
+	InvokerName string
+	InvokerUID  string
+}
+```
+```go
+// MsgHandler is the custom type provided to easily handle notifytextmessage events
+type MsgHandler func(m Message) (string, error)
+```
+
+### Login
+The Login method dials the TeamSpeak server and attempts to login using the provided credentials, if successful, it will set the server using the 'use' command
+```go
+func (b *bot) Login() error
+```
+
+### ConfigureMessageEvents
+ConfigureMessageEvents configures the bot to receive message alerts in text channels and sets the message handler function if not nil. If no message handler is provided then the message data is logged to standard output
+```go
+func (b *bot) ConfigureMessageEvents(h MsgHandler) error
+```
+
+### Start/Stop
+The Start method spawns the listener routine to accept data messages from the TeamSpeak server, this method is blocking and will wait until an os.Interrupt signal is received. The Start method handles proper cleanup and closes the connection to the server
+```go
+func (b *bot) Start() error
+```
 
 ## Basic Example
+A basic working example of a bot that parses two commands responding to both
 ```go
 package main
 
@@ -12,13 +66,13 @@ import (
 	"log"
 	"strings"
 
-	"github.com/jake-young-dev/tsgo"
+	tsgo "github.com/jake-young-dev/tsgo"
 )
 
 func main() {
 	bot, err := tsgo.New(tsgo.Config{
-		Address:  "127.0.0.1",
-		Port:     "10011",
+		Address:  "ip/url",
+		Port:     "port",
 		Username: "serveradmin",
 		Password: "password",
 		Server:   1,
@@ -27,7 +81,12 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bot.AddHandler(func(m tsgo.Message) (string, error) {
+	err = bot.Login()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = bot.ConfigureMessageEvents(func(m tsgo.Message) (string, error) {
 		fmt.Printf("%s: %s\n", m.InvokerName, m.Msg)
 		if strings.HasPrefix(m.Msg, ".") {
 			switch m.Msg {
@@ -39,10 +98,13 @@ func main() {
 		}
 		return "", nil
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	if err := bot.Start(); err != nil {
+	err = bot.Start()
+	if err != nil {
 		log.Fatal(err)
 	}
 }
-
 ```
--- ts.go
+++ ts.go
@@ -13,27 +13,21 @@ import (
 	"os/signal"
 	"strconv"
 	"strings"
-	"time"
 )
 
 const (
 	PROTOCOL = "tcp"
-	//on successful connection the server sends a two-line welcome banner
-	//we need to consume this to allow other messages to come through
+	//length of TeamSpeak welcome banner
 	BANNER_LENGTH = 2
-	//use to pass credentials to the TeamSpeak server
+	//TeamSpeak server login
 	LOGIN_STRING = "login %s %s"
-	//server command to enable message events in text channels, this allows
-	//the message alerts to come to the bot
+	//TeamSpeak command to enable message events from text channels
 	MSG_LISTENER_STRING = "servernotifyregister event=textchannel"
-	//determines which server the bot should use
+	//TeamSpeak command to select server
 	SERVER_USE_STRING = "use %d"
-	//the 'action' string for messages being sent in channel texts
+	//the 'action' string for messages being sent in channel texts, this
+	//is the type forwarded to the message handlers
 	MSG_ACTION = "notifytextmessage"
-	//the read deadline to be used on the underlying tcp connection, setting this
-	//prevents the read() call from hanging indefinitely and allows more graceful
-	//exits/disconnects
-	MSG_READ_DEADLINE = time.Minute * 1
 )
 
 var (
@@ -48,9 +42,8 @@ type response struct {
 	Data map[string]string
 }
 
-// Msg parses the text from a message alert, cleaning the data before returning. Message
-// strings utilize '\s' instead of whitespaces, these are replaced with proper whitespaces
-// before the message data is returned
+// Msg parses the text from a notifytextmessage event, cleaning the '\s' delimeters from the message
+// data before returning
 func (r *response) Msg() string {
 	msg, ok := r.Data["msg"]
 	if !ok {
@@ -61,22 +54,19 @@ func (r *response) Msg() string {
 }
 
 type Config struct {
-	//the server address and port to be used as the connection string to
-	//in the format <address>:<port>
+	//the server address and port of the TeamSpeak server
 	Address string
 	Port    string
-	//TeamSpeak credentials to authenticate the bot
+	//TeamSpeak credentials to authenticate the bot in the server
 	Username string
 	Password string
 	//the server identifier to be used in the 'use' command, this tells the
-	//bot which server to use. The port option is currently not supported by
-	//tsgo and the server ID must be used
+	//bot which server to listen to. TeamSpeak supports a port option as the identifier
+	//but it is not supported yet, this must be the Server ID
 	Server int
 }
 
-// Message contains the needed fields for notifytextmessage events
-
-// Message is the parsed fields from a notifytextmessage event, this
+// Message is the parsed message from a notifytextmessage event, this
 // struct is passed to the message handler functions
 type Message struct {
 	Msg         string
@@ -85,58 +75,121 @@ type Message struct {
 	InvokerUID  string
 }
 
-// Handler is the custom type provided to easily handle notifytextmessage events,
-// the raw server string is parsed into a Message struct and forwarded to the
-// handler functions. If a string is returned, and no error is encountered, then
-// the returned string will be written back to the server as a response
-type Handler func(m Message) (string, error)
+// MsgHandler is the custom type provided to easily handle notifytextmessage events
+type MsgHandler func(m Message) (string, error)
 
-type tsBot struct {
-	server net.Conn
-	reader *bufio.Scanner
-	//used to trigger bot shutdown
-	kill chan os.Signal
-	//essentially used as a waitgroup to ensure the listener routine
-	//finishes before the bot fully shuts down
-	clean   chan struct{}
-	cfg     Config
-	handler Handler
+type bot struct {
+	server     net.Conn
+	reader     *bufio.Scanner
+	kill       chan os.Signal
+	clean      chan struct{}
+	cfg        Config
+	msgHandler MsgHandler
 }
 
-type TsBot interface {
+type Bot interface {
+	Login() error
+	ConfigureMessageEvents(h MsgHandler) error
+	Start() error
+
 	read() (string, error)
 	write(msg string) error
-	listen() error
-	Start() error
-	login() error
 	close() error
 	parseResponse(res string) (*response, error)
-	AddHandler(f Handler)
+	login() error
+	listen() error
 }
 
 // New creates a new bot instance, verifying that the required configuration fields are present before
 // continuing. The bot requires address, port, username, and password to properly function
-func New(cfg Config) (TsBot, error) {
+func New(cfg Config) (Bot, error) {
 	if cfg.Address == "" || cfg.Password == "" || cfg.Username == "" || cfg.Port == "" {
 		return nil, errors.New("invalid configuration, ensure all fields are present")
 	}
 	k := make(chan os.Signal, 1)
-	signal.Notify(k, os.Interrupt)
-	return &tsBot{
+	return &bot{
 		kill:  k,
 		cfg:   cfg,
-		clean: make(chan struct{}),
+		clean: make(chan struct{}, 1),
+		msgHandler: func(m Message) (string, error) {
+			fmt.Printf("%s: %s\n", m.InvokerName, m.Msg)
+			return "", nil
+		},
 	}, nil
 }
 
-// read attempts to read data from the server connection, if data is present, it is returned
-func (t *tsBot) read() (string, error) {
-	if t.server == nil || t.reader == nil {
+// Login dials the TeamSpeak server and initializes the connection reader. TeamSpeak sends a welcome banner
+// on successful connection, it is automatically before sending the authentication and server selection commands
+func (b *bot) Login() error {
+	conn, err := net.Dial(PROTOCOL, net.JoinHostPort(b.cfg.Address, b.cfg.Port))
+	if err != nil {
+		return err
+	}
+
+	b.server = conn
+	b.reader = bufio.NewScanner(conn)
+
+	//read teamspeak 'welcome' banner
+	for x := 0; x < BANNER_LENGTH; x++ {
+		if _, err := b.read(); err != nil {
+			return err
+		}
+	}
+
+	err = b.login()
+	if err != nil {
+		return err
+	}
+
+	return b.writeSuccess(fmt.Sprintf(SERVER_USE_STRING, b.cfg.Server))
+}
+
+// ConfigureMessageEvents applies a custom message handler, if provided, and configures the TeamSpeak server
+// to send message events to the bot
+func (b *bot) ConfigureMessageEvents(h MsgHandler) error {
+	if h != nil {
+		b.msgHandler = h
+	}
+
+	err := b.writeSuccess(MSG_LISTENER_STRING)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Start spawns listener routine then waits for system interrupt. Once interrupt is received
+// the listener routine is killed and cleaned up closing the connection to the server
+func (b *bot) Start() error {
+	err := b.listen()
+	if err != nil {
+		return err
+	}
+
+	//wait for initial interrupt
+	wait := make(chan os.Signal, 1)
+	signal.Notify(wait, os.Interrupt)
+	<-wait
+
+	//forward interrupt to allow the listener routine loop to exit
+	b.kill <- os.Interrupt
+	err = b.close()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// read data from the server
+func (b *bot) read() (string, error) {
+	if b.server == nil || b.reader == nil {
 		return "", ErrNotConnected
 	}
 
-	if t.reader.Scan() {
-		return t.reader.Text(), nil
+	if b.reader.Scan() {
+		return b.reader.Text(), nil
 	}
 
 	return "", nil
@@ -144,164 +197,43 @@ func (t *tsBot) read() (string, error) {
 
 // write handles writing messages back to the remote server. All TeamSpeak messages must end
 // in a newline character, it is automatically added if it is missing
-func (t *tsBot) write(msg string) error {
-	if t.server == nil || t.reader == nil {
+func (b *bot) write(msg string) error {
+	if b.server == nil || b.reader == nil {
 		return ErrNotConnected
 	}
 
-	//ensure the newline suffix is present before sending
 	if !strings.HasSuffix(msg, "\n") {
 		msg += "\n"
 	}
 
-	//messages must end in newlines, this should probably eventually check for it before adding it
-	_, err := t.server.Write([]byte(msg))
+	_, err := b.server.Write([]byte(msg))
 	return err
-}
-
-// listen configures the server to receive message events. To do this two messages are sent to the server: first is to configure
-// which TeamSpeak server the bot should attach itself to, the second configures the bot to listen for notifytextmessage events.
-// If configuration is successful, a listener routine is spawned to continuously parse and handle messages until the bot is shutdown
-func (t *tsBot) listen() error {
-	err := t.writeSuccess(fmt.Sprintf(SERVER_USE_STRING, t.cfg.Server))
-	if err != nil {
-		return err
-	}
-
-	err = t.writeSuccess(MSG_LISTENER_STRING)
-	if err != nil {
-		return err
-	}
-
-	//a listener routine that loops until the kill channel is hit, parsing all messages and responding as
-	//needed. A read deadline is set after each read to prevent locking, the clean channel will be hit once
-	//the routine has finished completely
-	go func() {
-		for len(t.kill) == 0 {
-			t.server.SetReadDeadline(time.Now().Add(MSG_READ_DEADLINE))
-			res, err := t.read()
-			if os.IsTimeout(err) {
-				continue
-			}
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-
-			if res == "" || res == "\n" {
-				continue
-			}
-
-			r, err := t.parseResponse(res)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-
-			//prevent the bot from responding to itself
-			if r.Data["invokeruid"] == t.cfg.Username {
-				continue
-			}
-
-			if r.Action == MSG_ACTION {
-				invId, err := strconv.Atoi(r.Data["invokerid"])
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-
-				reply, err := t.handler(Message{
-					Msg:         r.Msg(),
-					InvokerID:   invId,
-					InvokerName: r.Data["invokername"],
-					InvokerUID:  r.Data["invokeruid"],
-				})
-				if err != nil {
-					fmt.Println(err)
-					continue
-				}
-
-				if reply != "" {
-					err := t.write(fmt.Sprintf("sendtextmessage targetmode=2 target=%d msg=%s", invId, strings.ReplaceAll(reply, " ", "\\s")))
-					if err != nil {
-						fmt.Println(err)
-						continue
-					}
-				}
-			}
-		}
-		t.clean <- struct{}{}
-	}()
-
-	return nil
-}
-
-// Start creates the tcp connection to the teamspeak server and initializes the scanner to read from the server. On successful
-// connection it will read two lines to consume the welcome banner to prevent errors parsing irregular messages. A login attempt
-// is the made and, if successful, listen for messages. This is a blocking call that will listen for messages until the kill channel
-// is hit, it will then handle safe shutdown disconnecting from the server
-func (t *tsBot) Start() error {
-	conn, err := net.Dial(PROTOCOL, net.JoinHostPort(t.cfg.Address, t.cfg.Port))
-	if err != nil {
-		return err
-	}
-
-	t.server = conn
-	t.reader = bufio.NewScanner(conn)
-
-	//read teamspeak 'welcome' banner
-	for x := 0; x < BANNER_LENGTH; x++ {
-		if _, err := t.read(); err != nil {
-			return err
-		}
-	}
-
-	err = t.login()
-	if err != nil {
-		return err
-	}
-
-	err = t.listen()
-	if err != nil {
-		return err
-	}
-
-	<-t.kill
-	//resend out the interrupt signal as an added measure since the above line will consume the
-	//initial one. This is an added failsafe to encourage the go routine to clean itself up
-	t.kill <- os.Interrupt
-	return t.close()
-}
-
-// login send the authentication message to the server, erroring if login was not successful
-func (t *tsBot) login() error {
-	return t.writeSuccess(fmt.Sprintf(LOGIN_STRING, t.cfg.Username, t.cfg.Password))
 }
 
 // close handles proper bot shutdown, the underlying tcp connection is closed to timeout any read calls
-// waiting in the listener routine. This allows the routine to be cleaned up without waiting for the read
-// deadline to be triggered, close will wait for the routine to signal its cleaned up before returning
-func (t *tsBot) close() error {
-	err := t.server.Close()
-	<-t.clean
-	return err
+// waiting in the listener routine
+func (b *bot) close() error {
+	err := b.server.Close()
+	if err != nil {
+		return err
+	}
+	<-b.clean
+	return nil
 }
 
-// writeSuccess writes data to this server and checks for a successful response. The server reply
-// is then parsed and checked for an "ok" field of an error action, returning an error if an error
-// is found
-func (t *tsBot) writeSuccess(msg string) error {
-	err := t.write(msg)
+// writeSuccess writes data to the server then checks for a success response
+func (b *bot) writeSuccess(msg string) error {
+	err := b.write(msg)
 	if err != nil {
 		return err
 	}
 
-	res, err := t.read()
+	res, err := b.read()
 	if err != nil {
 		return err
 	}
 
-	parsed, err := t.parseResponse(res)
+	parsed, err := b.parseResponse(res)
 	if err != nil {
 		return fmt.Errorf("%e, msg: %s", err, res)
 	}
@@ -318,10 +250,10 @@ func (t *tsBot) writeSuccess(msg string) error {
 	return nil
 }
 
-// parseResponse takes the string received from the server and unmarshal'd into
+// parseResponse takes the string received from the server and unmarshal's it into
 // the response struct for easier consumption. Messages from the server are seperated
-// by whitespaces and contain key=value fields
-func (t *tsBot) parseResponse(res string) (*response, error) {
+// by whitespaces and contain key=value fields after the action token
+func (b *bot) parseResponse(res string) (*response, error) {
 	fields := strings.Fields(res)
 	if len(fields) == 0 {
 		return nil, nil
@@ -346,8 +278,70 @@ func (t *tsBot) parseResponse(res string) (*response, error) {
 	return r, nil
 }
 
-// AddHandler configures the message handler function to forward server
-// messages to
-func (t *tsBot) AddHandler(f Handler) {
-	t.handler = f
+// login sends the authentication message to the server
+func (b *bot) login() error {
+	return b.writeSuccess(fmt.Sprintf(LOGIN_STRING, b.cfg.Username, b.cfg.Password))
+}
+
+// listen spawns a go routine to continuously read data from the server
+func (b *bot) listen() error {
+	go func() {
+		for len(b.kill) == 0 {
+			res, err := b.read()
+			if os.IsTimeout(err) {
+				continue
+			}
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+
+			if res == "" || res == "\n" {
+				continue
+			}
+
+			r, err := b.parseResponse(res)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+
+			//prevent the bot from responding to itself, this can get weird when using names outside
+			//of serveradmin or specific account names. It may need to be hardcoded
+			if r.Data["invokeruid"] == b.cfg.Username {
+				continue
+			}
+
+			if r.Action == MSG_ACTION {
+				invId, err := strconv.Atoi(r.Data["invokerid"])
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+
+				reply, err := b.msgHandler(Message{
+					Msg:         r.Msg(),
+					InvokerID:   invId,
+					InvokerName: r.Data["invokername"],
+					InvokerUID:  r.Data["invokeruid"],
+				})
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+
+				if reply != "" {
+					//move the command string to a const
+					err := b.write(fmt.Sprintf("sendtextmessage targetmode=2 target=%d msg=%s", invId, strings.ReplaceAll(reply, " ", "\\s")))
+					if err != nil {
+						fmt.Println(err)
+						continue
+					}
+				}
+			}
+		}
+		b.clean <- struct{}{}
+	}()
+
+	return nil
 }
